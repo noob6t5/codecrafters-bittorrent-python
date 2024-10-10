992a7618919fb989ce20d71bcc50375c3d88dde1
Brahma <upadhyayasangharsha@gmail.com>
2024-10-10 20:12:11 +0545

    MN6

--- app/main.py
+++ app/main.py
@@ -1,58 +1,86 @@
 import json
 import sys
+from typing import Any, List, Dict
 
 
-def decode_bencode(bencoded_value, index=0):
-    if bencoded_value[index] == ord("i"):  # Check for an integer
-        end_index = bencoded_value.find(b"e", index)
+class bencodeDecoder:
+    
+    def __init__(self, bencoded_value: bytes):
+        self.bencoded_value = bencoded_value
+        self.index = 0
+
+    def decode(self) -> Any:
+        return self._decode_types()
+
+    def _decode_intgr(self) -> int:
+        start_index = self.index + 1
+        end_index = self.bencoded_value.find(b"e", start_index)
         if end_index == -1:
-            raise ValueError("Invalid encoded integer")
-        return (int(bencoded_value[index + 1 : end_index]),end_index + 1,) 
+            raise ValueError("'e' isn't there. Invalid Integer")
+        intgr_value = int(self.bencoded_value[start_index:end_index])
+        self.index = end_index + 1
+        return intgr_value
 
-    elif bencoded_value[index] == ord("l"):  # Check for a list
-        result_list = []
-        index += 1  
-        while index < len(bencoded_value) and bencoded_value[index] != ord("e" ):  
-            element, new_index = decode_bencode(
-                bencoded_value, index
-            )  # Decode the element
-            result_list.append(element)  # Append 
-            index = new_index  
-        if index >= len(bencoded_value) or bencoded_value[index] != ord("e"):
-            raise ValueError("Invalid encoded list")   
-
-        return result_list, index + 1  
-
-    elif chr(bencoded_value[index]).isdigit():  
-        first_colon_index = bencoded_value.find(b":", index)
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        length = int(bencoded_value[index:first_colon_index])  # Length of the string
-        start_index = first_colon_index + 1
+    def _decode_str(self) -> str:
+        fst_index = self.bencoded_value.find(b":", self.index)
+        if fst_index == -1:
+            raise ValueError("':' isn't there. Invalid String")
+        length = int(self.bencoded_value[self.index : fst_index])
+        start_index = fst_index + 1
         end_index = start_index + length
-        if end_index > len(bencoded_value):
-            raise ValueError("Invalid string length")
-        return (bencoded_value[start_index:end_index], end_index,)  
-    else:
-        raise NotImplementedError(
-            "Error"
-        )
+        if end_index > len(self.bencoded_value):
+            raise ValueError("More length error.")
+        str_value = self.bencoded_value[start_index:end_index]
+        self.index = end_index
+        return str_value.decode()  
 
+    def _decode_list(self) -> List[Any]:
+        rst_list = []
+        self.index += 1
+        while self.index < len(self.bencoded_value) and self.bencoded_value[self.index]!= ord("e"):
+            elem = self._decode_types()
+            rst_list.append(elem)
+        if self.index >= len(self.bencoded_value) or self.bencoded_value[self.index]!= ord("e"):
+            raise ValueError("'e' missing.")
+        self.index += 1
+        return rst_list
 
-def main():
-    command = sys.argv[1]
-    if command == "decode":
-        bencoded_value = sys.argv[2].encode()
+    def _decode_dict(self) -> Dict[str, Any]:
+        result_dict = {}
+        self.index += 1
+        while self.index < len(self.bencoded_value) and self.bencoded_value[self.index] != ord("e"):
+            key = self._decode_str()
+            value = self._decode_types()
+            result_dict[key] = value
+        if self.index >=len(self.bencoded_value) or self.bencoded_value[self.index] != ord("e"):
+            raise ValueError("'e' missing. Invalid Dictionary")
+        self.index+= 1
+        return result_dict
 
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
-            raise TypeError(f"Type not serializable: {type(data)}")
-        decoded_value, _ = decode_bencode(bencoded_value)  # Start decoding from index 0
-        print(json.dumps(decoded_value, default=bytes_to_str))  
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
+    def _decode_types(self) -> Any:
+        if self.bencoded_value[self.index]== ord("i"):
+            return self._decode_intgr()
+        if self.bencoded_value[self.index]== ord("l"):
+            return self._decode_list()
+        if self.bencoded_value[self.index]== ord("d"):
+            return self._decode_dict()
+        elif chr(self.bencoded_value[self.index]).isdigit():
+            return self._decode_str()
+        else:
+            raise NotImplementedError("Include Only Integers, String, Dict & List")
 
 
 if __name__ == "__main__":
-    main()
+    command = sys.argv[1]
+    bencoded_inp = sys.argv[2].encode()
+
+    if command == "decode":
+        decoder = bencodeDecoder(bencoded_inp)
+        try:
+            decoded_obj = decoder.decode()
+            print(json.dumps(decoded_obj, indent=3))
+        except (ValueError, NotImplementedError) as e:
+            print(f"Error: {e}")
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
