2fd3545e0cfe70d1b8bde92c118ba7aab3961ac6
Brahma <upadhyayasangharsha@gmail.com>
2024-10-10 14:09:22 +0545

    AH1

--- app/main.py
+++ app/main.py
@@ -2,20 +2,40 @@ import json
 import sys
 
 
-def decode_bencode(bencoded_value):
-    if bencoded_value[0] == ord("i"):  # Check if the value is a bencoded integer
-        end_index = bencoded_value.find(b"e")
+def decode_bencode(bencoded_value, index=0):
+    if bencoded_value[index] == ord("i"):  # Check for an integer
+        end_index = bencoded_value.find(b"e", index)
         if end_index == -1:
             raise ValueError("Invalid encoded integer")
-        return int(bencoded_value[1:end_index])  
-    elif chr(bencoded_value[0]).isdigit():  
-        first_colon_index = bencoded_value.find(b":")
+        return (int(bencoded_value[index + 1 : end_index]),end_index + 1,) 
+
+    elif bencoded_value[index] == ord("l"):  # Check for a list
+        result_list = []
+        index += 1  
+        while index < len(bencoded_value) and bencoded_value[index] != ord("e" ):  
+            element, new_index = decode_bencode(
+                bencoded_value, index
+            )  # Decode the element
+            result_list.append(element)  # Append 
+            index = new_index  
+        if index >= len(bencoded_value) or bencoded_value[index] != ord("e"):
+            raise ValueError("Invalid encoded list")   
+
+        return result_list, index + 1  
+
+    elif chr(bencoded_value[index]).isdigit():  
+        first_colon_index = bencoded_value.find(b":", index)
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index + 1 :]
+        length = int(bencoded_value[index:first_colon_index])  # Length of the string
+        start_index = first_colon_index + 1
+        end_index = start_index + length
+        if end_index > len(bencoded_value):
+            raise ValueError("Invalid string length")
+        return (bencoded_value[start_index:end_index], end_index,)  
     else:
         raise NotImplementedError(
-            "Only strings and integers are supported at the moment"
+            "Error"
         )
 
 
@@ -23,12 +43,13 @@ def main():
     command = sys.argv[1]
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
+
         def bytes_to_str(data):
             if isinstance(data, bytes):
                 return data.decode()
             raise TypeError(f"Type not serializable: {type(data)}")
-
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        decoded_value, _ = decode_bencode(bencoded_value)  # Start decoding from index 0
+        print(json.dumps(decoded_value, default=bytes_to_str))  
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
